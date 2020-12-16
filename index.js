40308fa0774afa1cf2ff77f25c4d608824ab91c4
bcoq <ah2014bh@outlook.com>
2020-12-16 07:54:15 +0300

    Simple chat with bot

---
+++ index.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const config = require('./config.json')
+const alexa = require('alexa-bot-api')
+const ai = new alexa('aw2plm')
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on('message', message => {
+    if (message.channel.type == 'dm') return
+    let chatChannel = message.guild.channels.cache.find((x) => x.name == 'chat-with-bot')
+    if (message.channel == chatChannel) {
+        if (message.author.bot) return
+        let content = message.content
+        ai.getReply(content).then((r) => message.channel.send(r))
+    }
+})
+
+client.login(config.token)
